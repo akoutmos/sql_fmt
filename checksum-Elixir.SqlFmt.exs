9dba844b46c9586f0a82289b99e17d0cc94b4e14
Alex Koutmos <akoutmos@gmail.com>
2024-10-05 00:35:48 -0400

    CI work

---
+++ checksum-Elixir.SqlFmt.exs
@@ -0,0 +1,12 @@
+%{
+  "libsql_formatter-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:19644ca9d42f9f31ab798781003316b15fc4a3ab7940fafebffd2604934225e9",
+  "libsql_formatter-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:3c417efb26e5296f18cc8c1cf9f0ec8dd0f5a4b1b6133bddc482ce6df0b19388",
+  "libsql_formatter-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:b9c42907febfd4dedcb66395a7eff6a8c5c5b54a3042bf1a1a20f9a7d4ff884f",
+  "libsql_formatter-v0.1.0-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:addfce3a2bbc7ab1b076b3cc50511cd89ebb07c109309c02889249edb82aad43",
+  "libsql_formatter-v0.1.0-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:97fc84064eb81f3b727d86706a936be2212739cabcfd4069f29385dbb614001c",
+  "libsql_formatter-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:be5d518b7565e1eb28b4f777395f4dbea54b67f6d279f817c0d8a06f797f8be2",
+  "libsql_formatter-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c85fc1a9801bf786473f5769f504492453cf40ceb17b4972fd86466bb2e23106",
+  "libsql_formatter-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:4075a52c1791ef728b4c40e162b8c274d514769c9f2feea009927951a2ee3248",
+  "sql_formatter-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:91c2d3f1721c68047e18f3b68f38244433898d52dccbee29334e89bb8abee8a6",
+  "sql_formatter-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:4248c21b5cc31824622c50dc727bfcf783229b40364da18eb2d3c7ed4c75183e",
+}
--- config/config.exs
+++
@@ -1,3 +0,0 @@
-import Config
-
-config :rustler_precompiled, :force_build, sql_fmt: true
