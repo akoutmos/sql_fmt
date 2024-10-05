89efd46b396840934b53c4670c61f935632e02e7
Alex Koutmos <akoutmos@gmail.com>
2024-10-04 21:25:08 -0400

    Initial commit

---
+++ lib/sql_fmt.ex
@@ -0,0 +1,18 @@
+defmodule SqlFmt do
+  @moduledoc """
+  Documentation for `SqlFmt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SqlFmt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sql_fmt_test.exs
@@ -0,0 +1,8 @@
+defmodule SqlFmtTest do
+  use ExUnit.Case
+  doctest SqlFmt
+
+  test "greets the world" do
+    assert SqlFmt.hello() == :world
+  end
+end
