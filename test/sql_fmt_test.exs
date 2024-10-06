9311d6a8391365c5ebe949e5fa689755b2b75848
Alex Koutmos <akoutmos@gmail.com>
2024-10-06 16:11:09 -0400

    CI work

--- test/sql_fmt_test.exs
+++ test/sql_fmt_test.exs
@@ -2,7 +2,7 @@ defmodule SqlFmtTest do
   use ExUnit.Case
 
   describe "SqlFmt.format_query/2" do
-    test "should format the query" do
+    test "should format the query with default formatting options" do
       assert {:ok, result} = SqlFmt.format_query("select * from hello_world;")
 
       assert String.trim("""
@@ -10,6 +10,27 @@ defmodule SqlFmtTest do
                *
              FROM
                hello_world;
+             """) == result
+    end
+
+    test "should format the query with custom formatting options" do
+      assert {:ok, result} = SqlFmt.format_query("select * from hello_world;", indent: 4, uppercase: false)
+
+      assert String.trim("""
+             select
+                 *
+             from
+                 hello_world;
+             """) == result
+
+      assert {:ok, result} =
+               SqlFmt.format_query("select * from hello_world;", indent: 4, ignore_case_convert: ["select"])
+
+      assert String.trim("""
+             select
+                 *
+             FROM
+                 hello_world;
              """) == result
     end
   end
