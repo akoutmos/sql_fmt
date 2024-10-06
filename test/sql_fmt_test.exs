d5f9e070506ea29b97039cdc9518424e079ba006
Alex Koutmos <akoutmos@gmail.com>
2024-10-06 16:14:12 -0400

    CI work

--- test/sql_fmt_test.exs
+++ test/sql_fmt_test.exs
@@ -34,4 +34,51 @@ defmodule SqlFmtTest do
              """) == result
     end
   end
+
+  describe "SqlFmt.format_query_with_params/2" do
+    test "should format the query with default formatting options" do
+      assert {:ok, result} = SqlFmt.format_query_with_params("select * from hello_world where thing = ?;", ["1"])
+
+      assert String.trim("""
+             SELECT
+               *
+             FROM
+               hello_world
+             WHERE
+               thing = 1;
+             """) == result
+    end
+
+    test "should format the query with custom formatting options" do
+      assert {:ok, result} =
+               SqlFmt.format_query_with_params("select * from hello_world where thing = ?;", ["1"],
+                 indent: 4,
+                 uppercase: false
+               )
+
+      assert String.trim("""
+             select
+                 *
+             from
+                 hello_world
+             where
+                 thing = 1;
+             """) == result
+
+      assert {:ok, result} =
+               SqlFmt.format_query_with_params("select * from hello_world where thing = ?;", ["1"],
+                 indent: 4,
+                 ignore_case_convert: ["select"]
+               )
+
+      assert String.trim("""
+             select
+                 *
+             FROM
+                 hello_world
+             WHERE
+                 thing = 1;
+             """) == result
+    end
+  end
 end
