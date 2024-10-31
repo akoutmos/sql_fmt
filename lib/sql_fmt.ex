c05f68cfcbc80d59ee7e6988808ed5e01dae09ed
Daniel Kukula <daniel.kuku@gmail.com>
2024-11-01 00:05:44 +0100

    run_character_replacement_on_rust_side

--- lib/sql_fmt.ex
+++ lib/sql_fmt.ex
@@ -19,9 +19,7 @@ defmodule SqlFmt do
   def format_query(query, fmt_opts \\ []) do
     format_options = FormatOptions.new(fmt_opts)
 
-    query
-    |> Native.format(format_options)
-    |> maybe_clean_special_character_operators()
+    Native.format(query, format_options)
   end
 
   @doc """
@@ -39,38 +37,6 @@ defmodule SqlFmt do
   def format_query_with_params(query, query_params, fmt_opts \\ []) do
     format_options = FormatOptions.new(fmt_opts)
 
-    query
-    |> Native.format(query_params, format_options)
-    |> maybe_clean_special_character_operators()
-  end
-
-  # ---- Private helper functions ----
-
-  @special_character_sequence_regex ~r/(\+|\-|\*|\/|\<|\>|\=|\~|\!|\@|\#|\%|\^|\&|\||\`|\?)\s(\+|\-|\*|\/|\<|\>|\=|\~|\!|\@|\#|\%|\^|\&|\||\`|\?)/
-
-  # This is a work around the following bug in the Rust formatting
-  # library https://github.com/shssoichiro/sqlformat-rs/issues/52
-  #
-  # 63 Regex replacement passes has been chosen as it is the default max
-  # operator name as per the Postgres docs https://www.postgresql.org/docs/current/sql-createoperator.html.
-  # The function will break early if no more replacements need to occur though.
-  defp maybe_clean_special_character_operators({:ok, formatted_query}) do
-    post_processed_query =
-      1..63
-      |> Enum.reduce_while(formatted_query, fn _pass, acc ->
-        new_string = Regex.replace(@special_character_sequence_regex, acc, "\\g{1}\\g{2}")
-
-        if acc == new_string do
-          {:halt, new_string}
-        else
-          {:cont, new_string}
-        end
-      end)
-
-    {:ok, post_processed_query}
-  end
-
-  defp maybe_clean_special_character_operators(other) do
-    other
+    Native.format(query, query_params, format_options)
   end
 end
--- native/sql_fmt_nif/src/lib.rs
+++ native/sql_fmt_nif/src/lib.rs
@@ -1,6 +1,13 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use rustler::Atom;
 use rustler::NifStruct;
 use rustler::NifTuple;
+
+lazy_static! {
+    static ref OPERATOR_REGEX: Regex =
+        Regex::new(r"([+\-*/<=>\~!@#%^&|`?])\s([+\-*/<=>\~!@#%^&|`?])").unwrap();
+}
 
 use sqlformat::{FormatOptions, Indent, QueryParams};
 
@@ -36,10 +43,11 @@ fn format(sql_query: String, format_options: ElixirFormatOptions) -> StringResul
     };
 
     let formatted_sql = sqlformat::format(sql_query.as_str(), &QueryParams::None, &options);
+    let post_process_query = post_process_query(formatted_sql);
 
     return StringResultTuple {
         lhs: atoms::ok(),
-        rhs: formatted_sql.to_string(),
+        rhs: post_process_query.to_string(),
     };
 }
 
@@ -61,11 +69,39 @@ fn format(
         &QueryParams::Indexed(query_params),
         &options,
     );
+    let post_process_query = post_process_query(formatted_sql);
 
     return StringResultTuple {
         lhs: atoms::ok(),
-        rhs: formatted_sql.to_string(),
+        rhs: post_process_query.to_string(),
     };
+}
+
+// ---- Private helper functions ----
+
+// This is a work around the following bug in the Rust formatting
+// library https://github.com/shssoichiro/sqlformat-rs/issues/52
+//
+// 63 Regex replacement passes has been chosen as it is the default max
+// operator name as per the Postgres docs https://www.postgresql.org/docs/current/sql-createoperator.html.
+// The function will break early if no more replacements need to occur though.
+
+fn post_process_query(formatted_query: String) -> String {
+    let mut current_string = formatted_query;
+
+    for _ in 0..63 {
+        let new_string = OPERATOR_REGEX
+            .replace_all(&current_string, "$1$2")
+            .to_string();
+
+        if current_string == new_string {
+            return new_string;
+        }
+
+        current_string = new_string;
+    }
+
+    current_string
 }
 
 rustler::init!("Elixir.SqlFmt.Native");
